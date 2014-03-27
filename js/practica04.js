15f9948f26a064354f921c5b2c9b0f6e58237585
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-03-27 21:13:47 +0100

    [practica 4] Recorrer las propiedades de un objeto, dibijando una tabla HTML con dos columnas: la 1ª para los atributos del objeto, la 2ª para los valores de los atributos

--- js/practica04.js
+++ js/practica04.js
@@ -1,3 +1,30 @@
+function closeTag(tag) {
+	return "</" + tag + "/>";
+}
+
+function drawTag(tag, content) {
+	return openTag(tag) + content + closeTag(tag);
+}
+
+function openTag(tag) {
+	return "<" + tag + "/>";
+}
+
+function printObject (o) {
+	var content = "";
+
+	for (field in o) {
+		var td1 = drawTag("td", field);
+		var td2 = drawTag("td", o[field]);
+
+		var tr = drawTag("tr", td1 + td2);
+
+		content += tr;
+	}
+
+	return drawTag("table", content);
+}
+
 function requestObject() {
 	var requestedObject = prompt("Indique el nombre de algún objeto JavaScript de la página actual:");
 
@@ -7,6 +34,6 @@ function requestObject() {
 		return "El objeto referenciado NO existe";
 	}
 	else {
-		return o; 
+		return printObject(o); 
 	}
 }
