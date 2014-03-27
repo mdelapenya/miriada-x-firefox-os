97e0acfe2aacb7f668eb345f9f9ddc17f3d9dec0
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-03-27 21:48:32 +0100

    [practica 4] Permitir pasar un objeto que represente atributos a pasar a un tag HTML

--- js/practica04.js
+++ js/practica04.js
@@ -2,33 +2,62 @@ function closeTag(tag) {
 	return "</" + tag + "/>";
 }
 
-function drawTag(tag, content) {
+function drawTag(tag, content, attributes) {
 	var type = typeof content;
 
 	if (type === "object" || type === "function") {
 		content = "No es imprimible";
 	}
 
-	return openTag(tag) + content + closeTag(tag);
+	return openTag(tag, attributes) + content + closeTag(tag);
 }
 
-function openTag(tag) {
-	return "<" + tag + "/>";
+function openTag(tag, attributes) {
+	var attr = processAttributes(attributes);
+
+	return "<" + tag + attr +" />";
 }
 
 function printObject (o) {
 	var content = "";
 
+	var count = 0;
+
 	for (field in o) {
+		var cssClassName = "odd";
+
+		if (count % 2 === 0) {
+			cssClassName = "even";
+		}
+
 		var td1 = drawTag("td", field);
 		var td2 = drawTag("td", o[field]);
 
-		var tr = drawTag("tr", td1 + td2);
+		var cssClass = {};
+		cssClass.class = cssClassName;
+
+		var tr = drawTag("tr", td1 + td2, cssClass);
 
 		content += tr;
+
+		count++;
 	}
 
 	return drawTag("table", content);
+}
+
+function processAttributes(attributes) {
+	var attr = "";
+
+	for (attribute in attributes) {
+		attr += " ";
+		attr += attribute;
+		attr += "='";
+		attr += attributes[attribute];
+		attr += "'";
+	}
+
+	return attr;
 }
 
 function requestObject() {
