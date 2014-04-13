199f2084d6fbf958cdce4c3c416168916876f222
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-04-13 19:12:04 +0200

    [opcional 5.2] Activar y desactivar el reloj al pulsar sobre él

--- js/eventos.js
+++ js/eventos.js
@@ -4,22 +4,40 @@ var clock = {
 	_minute: 0,
 	_second: 0,
 	_millisecond: 0,
+	_initialized: false,
+	_intervalId: 0,
 
 	display:function() {
 		var date = new Date();
 
-		this._hour = date.getHours();
-		this._minute = date.getMinutes();
-		this._second = date.getSeconds();
-		this._millisecond = date.getMilliseconds();
-		this._divClock = document.getElementById("clock");
+		clock._hour = date.getHours();
+		clock._minute = date.getMinutes();
+		clock._second = date.getSeconds();
+		clock._millisecond = date.getMilliseconds();
 
-		var html = this._hour + " horas, " + this._minute + " minutos, " + this._second + " segundos, " + this._millisecond + " ms.";
+		var html = clock._hour + " horas, " + clock._minute + " minutos, " + clock._second + " segundos, " + clock._millisecond + " ms.";
 
-		this._divClock.innerHTML = html;
+		clock._divClock.innerHTML = html;
+	},
+
+	toggle:function() {
+		if (clock._initialized === true) {
+			window.clearInterval(clock._intervalId);
+
+			clock._initialized = false;
+		}
+		else {
+			clock._intervalId = setInterval(clock.display, 1);
+
+			clock._initialized = true;
+		}
 	}
 }
 
 var initialize = function() {
-	setInterval(clock.display, 1);
+	var div = document.getElementById("clock");
+
+	clock._divClock = div;
+
+	div.onclick = clock.toggle;
 }
