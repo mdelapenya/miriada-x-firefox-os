35bcfbe90a8c59df019c7b149feb9a8afa4c6b2f
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-04-13 21:09:55 +0200

    [opcional 6.1] Crear un cronómetro

---
+++ js/cronometro.js
@@ -0,0 +1,38 @@
+var crono = {
+	_cl: $("#crono"),
+	_intervalId: undefined,
+
+	arrancar:function() {
+		crono._intervalId = setInterval(crono.mostrar, 100);
+	},
+
+	cambiar:function() {
+		if (!crono._intervalId) {
+			crono.arrancar();
+		}
+		else {
+			crono.parar();
+		}
+	},
+
+	mostrar:function() {
+		crono._cl.html((+crono._cl.html() + 0.1).toFixed(1));
+	},
+
+	parar:function() {
+		clearInterval(crono._intervalId);
+
+		crono._intervalId = undefined;
+	}
+}
+
+var initialize = function() {
+	$("#btnCambiar").on("click", crono.cambiar);
+
+	$("#btnInicializar").on(
+		"click",
+		function() {
+			crono._cl.html("0.0");
+		}
+	);
+}
