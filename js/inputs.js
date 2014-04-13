764699db3565ad479cfb038d9718a64f14252129
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-04-13 19:40:47 +0200

    [opcional 5.3] usaar un input test y un button para preguntar al usuario, mostrando en un div el resultado de la pregunta (correcto o incorrecto)

---
+++ js/inputs.js
@@ -0,0 +1,26 @@
+function check() {
+	// por simplicidad, meto todas las combinaciones de acentos y así no tener que reemplazar
+
+	var answers = ["cristobal colon", "cristóbal colón", "cristóbal colon", "cristobal colón"];
+
+	var inputAnswer = document.getElementById("txtAnswer");
+
+	var answer = inputAnswer.value;
+
+	var lowerCaseAnswer = answer.toLowerCase();
+
+	var result = "";
+
+	if ((lowerCaseAnswer === answers[0]) || lowerCaseAnswer === answers[1] ||
+		lowerCaseAnswer === answers[2] || lowerCaseAnswer === answers[3]) {
+
+		result = "Correcto, Cristóbal Colón descubrió América";
+	}
+	else {
+		result = "Su respuesta  " + answer + "  no es correcta";
+	}
+
+	var divResult = document.getElementById("resultado");
+
+	divResult.innerHTML = result;
+}
